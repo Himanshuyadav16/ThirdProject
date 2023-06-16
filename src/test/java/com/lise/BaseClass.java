15c82fbfda056a427c699fda8083ca5a2f8606be
Himanshu Yadav <himanshuyadav1607@gmail.com>
2023-06-16 23:34:47 +0530

    first commit

--- src/test/java/com/lise/BaseClass.java
+++ src/test/java/com/lise/BaseClass.java
@@ -1,4 +1,13 @@
 package com.lise;
 
+import com.lise.utils.ApplicationProperties;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeSuite;
+
 public class BaseClass {
+    @BeforeSuite
+    public void beforeSuite() {
+        RestAssured.baseURI = ApplicationProperties.INSTANCE.getUrl();
+        System.out.println("Before Suite");
+    }
 }
